1d4d35bdbdb6c820e6a58a8403b1659b6bbb9afe
Larik7 <Larik777taganka@gmail.com>
2023-04-25 07:58:06 +0300

    second commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { lazy } from 'react';
+import { Route, Routes } from 'react-router-dom'
+import { Layout } from './components/SharedLayout/SharedLayout';
+import './App.css'
+
+const HomePage = lazy(() => import('./pages/Home'))
+const Tweets = lazy(() => import('./pages/Tweets'))
+
+function App() {
+
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<HomePage />} />
+        <Route
+          path="/Tweets"
+          element={<Tweets />} />
+      </Route>
+    </Routes>
+  )
+}
+
+export default App
---
+++ src/components/Appbar/AppBar.jsx
@@ -0,0 +1,12 @@
+import { Navigation } from '../Navigation/Navigation';
+// import { useAuth } from 'hooks';
+// import css from './AppBar.module.css';
+
+export const AppBar = () => {
+
+  return (
+    <header>
+      <Navigation />
+    </header>
+  );
+};
