ce4a93dc1224c065c9733a2157387aab62b6875d
Larik7 <Larik777taganka@gmail.com>
2023-04-26 22:14:43 +0300

    sixth commit

--- src/redux/tweetsSlice.js
+++ src/redux/tweetsSlice.js
@@ -1,5 +1,5 @@
-import { createSlice } from '@reduxjs/toolkit';
-import { fetchTweets, editCounts } from './operations';
+import { createSlice, isAnyOf } from "@reduxjs/toolkit";
+import { fetchTweets, editCounts } from "./operations";
 
 const handlePending = state => {
   state.isLoading = true;
@@ -10,40 +10,47 @@ const handleRejected = (state, action) => {
   state.error = action.payload;
 };
 
-const tweetsSlice  = createSlice({
-  name: 'tweets',
-  initialState: {
+const initialState = {
     items: [],
     isLoading: false,
     error: null,
-  },
-  extraReducers: builder =>
-    builder
-      .addCase(fetchTweets.pending, handlePending)
-      .addCase(fetchTweets.fulfilled, (state, action) => {
-        state.isLoading = false;
-        state.error = null;
-        state.items = action.payload;
-      })
-      .addCase(fetchTweets.rejected, handleRejected)
-      .addCase(editCounts.pending, handlePending)
-      .addCase(editCounts.fulfilled, (state, action) => {
-        state.isLoading = false;
-        state.error = null;
-        state.items.push(action.payload);
-        state.items = state.items.map(item => {
-          if (item.id === action.payload.id) {
-            return {
-              ...item,
-              followers: action.payload.followers,
-              isFollowing: action.payload.isFollowing,
-            };
-          }
+}
 
-          return item;
-        });
-      })
-      .addCase(editCounts.rejected, handleRejected)
+const tweetsSlice = createSlice({
+    name: 'tweets',
+    initialState: initialState,
+    reducers: {
+        setFollow(state, action) {
+           for (const user of state.items) {
+                if (user.id === action.payload) {
+                    user.isFollowing = !user.isFollowing;
+                    break;
+                }
+            }
+        }
+    },
+    extraReducers: builder =>
+        builder
+            .addCase(fetchTweets.pending, handlePending)
+            .addCase(fetchTweets.fulfilled, (state, action) => {
+                state.isLoading = false;
+                state.error = null;
+                state.items = action.payload;
+            })
+            .addCase(editCounts.fulfilled, (state, action) => {
+                state.isLoading = false;
+                state.error = null;
+                state.items.forEach(item => {
+                    if (item.id !== action.payload.id) {
+                        return
+                    }
+                    item.followers = action.payload.followers;
+                })
+            })
+            .addMatcher(
+                isAnyOf(fetchTweets.rejected, editCounts.rejected), handleRejected
+            )
 });
 
 export const tweetsReducer = tweetsSlice.reducer;
+export const {setFollow} = tweetsSlice.actions;
